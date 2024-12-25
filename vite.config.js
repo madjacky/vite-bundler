9c0be75b1334ffd86ffc553a3d8b5ddd41a117b8
madjacky <jacky.iakovenko@gmail.com>
2024-12-25 13:30:43 +0400

    HTML/CSS(SCSS)/JAVASCRIPT project bundler on VITE 6 with Tailwindcss and Postcss

---
+++ vite.config.js
@@ -0,0 +1,30 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import injectHTML from 'vite-plugin-html-inject';
+
+export default defineConfig({
+  root: 'src',
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@js': resolve(__dirname, 'src/javascript'),
+      '@components': resolve(__dirname, 'src/javascript/components'),
+      '@functions': resolve(__dirname, 'src/javascript/functions'),
+      '@assets': resolve(__dirname, 'assets'),
+      '@scss': resolve(__dirname, 'src/scss'),
+    },
+  },
+  publicDir: '../assets',
+  plugins: [
+    injectHTML(),
+  ],
+})
